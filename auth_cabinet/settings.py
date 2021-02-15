d2ea7286e454a17532dbb5fbaf889788f3b971e2
Alexandr Agafonov <agafonovfm@gmail.com>
2021-02-15 13:35:39 +0300

    last commit

--- auth_cabinet/settings.py
+++ auth_cabinet/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '7!lz31k2i_1rbttt=$^b^hjno=nyn8)of!lcar%i@h2rnlle8q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
